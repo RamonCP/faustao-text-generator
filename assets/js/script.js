cacc7baee6fa1793e240a8eba7ae104f7845e62e
Ramon Cavalcante <ramon.pires@apllos.com>
2021-03-13 19:58:59 -0300

    Copy text to clipboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,19 @@ const quotes = new App()
 const $paragraph = document.querySelector('#paragraph')
 const $generate = document.querySelector('#generate')
 const $textGenerated = document.querySelector('#textGenerated')
+const $copyButton = document.querySelector('.copy')
 
 let paragraph = ""
 
-$generate.addEventListener('click',handleButton)
-
+$generate.addEventListener('click', handleButton)
+$copyButton.addEventListener('click', copyText)
 
 function handleButton() {
     paragraph = quotes.generateParagraph($paragraph.value)
     $textGenerated.innerHTML = paragraph
+}
+
+function copyText() {
+    $textGenerated.select()
+    document.execCommand('copy')
 }
